c3b36704a2ef058224e083ef2e7634a421f915b3
Imran <imranradziii@gmail.com>
2022-08-09 23:04:07 +0100

    added quicksand font, some initial content for the restaurant page (navbar) and some styling

--- src/index.js
+++ src/index.js
@@ -1,2 +1,27 @@
-console.log('hello there');
-console.log('testing');
+import './styles/index.css';
+
+const contentContainer = document.querySelector('#content');
+
+const options = document.createElement('div');
+options.setAttribute('id', 'options');
+
+const mainContent = document.createElement('div');
+mainContent.setAttribute('id', 'mainContent');
+mainContent.textContent = 'main content goes here'
+
+function optBtn(name) {
+  const div = document.createElement('div');
+  div.textContent = name;
+  div.setAttribute('id', name);
+  return div
+}
+
+const buttons = [optBtn('home'), optBtn('menu'), optBtn('contact')];
+
+contentContainer.appendChild(options);
+contentContainer.appendChild(mainContent);
+
+for (const button of buttons) {
+  options.appendChild(button);
+}
+
